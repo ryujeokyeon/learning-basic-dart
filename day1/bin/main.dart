36a9810d1d1695fcbefdc708c94611fa5a005cf7
Annisa Fitriani Haqiqi <annisafitrianihaqiqi@gmail.com>
2023-01-09 14:24:45 +0700

    day 1 learning basic dart

---
+++ day1/bin/main.dart
@@ -0,0 +1,65 @@
+// void main() {
+//     var myName = 'Annisa';
+//     print(myName);
+// }
+
+// void main() {
+//   String namaAwal = 'Annisa';
+//   print(namaAwal);
+// }
+
+// void main() {
+//   int jumlahBuku = 14;
+//   print(jumlahBuku);
+// }
+
+// void main() {
+//   double tinggiBadan = 184.5;
+//   print(tinggiBadan);
+// }
+
+// void main() {
+//   num angka = 4.5;
+//   print(angka);
+// }
+
+// void main() {
+//   bool tes;
+//   tes = 21 < 20;
+//   print(tes);
+// }
+
+// void main() {
+//   List dataList = [99, 1.4, 'a', 'List'];
+//   print(dataList);
+// }
+
+// void main() {
+//   Map<String, dynamic> dataMap = {
+//     "url": "https://twitter.com/ryujeokyeon",
+//     "directName": "an",
+//     "penulis": "Annisa",
+//   };
+//   print(dataMap);
+// }
+
+// void main() {
+//   dynamic a = 1.5;
+//   a = "halo dunia!";
+//   print(a);
+// }
+
+// void main() {
+//   var nama = 'Annisa';
+//   var angka = 114;
+//   var tinggi = 160.5;
+//   print(nama);
+//   print(angka);
+//   print(tinggi);
+// }
+
+// void main() {
+//   final dataFinal = 'Anime 2023';
+//   dataFinal = 'Anime 2024';
+//   print(dataFinal);
+// }
